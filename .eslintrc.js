62999b866a14d78943fb0b1e0fadc01955eb3014
Syed <syedfh4@gmail.com>
2018-03-21 14:46:27 -0700

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+          "off"
+        ]
+    }
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+var http = require('http');
+//File system module
+var fs = require('fs');
+//Imported custom module using the require function.
+//Assigned the value of the module to the new variable extract.
+var extract = require('./extract');
+var wss = require('./websockets-server');
+var mime = require('mime');
+
+//check for a file error and write a 404 error code if one is found
+var handleError = function (err, res) {
+	res.writeHead(404);
+	res.end();
+};
+
+var server = http.createServer(function (req, res) {
+	console.log('Responding to a request.');
+	////Now able to use the extract function just as the extractFilePath function
+	var filePath = extract(req.url);
+	fs.readFile(filePath, function (err, data) {
+		if (err) {
+			handleError(err, res);
+			return;
+		} else {
+			var contentType = mime.getType(filePath);
+
+			res.setHeader('Content-Type', contentType);
+			res.end(data);
+		}
+	});
+});
+server.listen(3000);
